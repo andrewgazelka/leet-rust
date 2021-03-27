1f3a0dc884992e5481a32875e49969307bc5fc82
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-03-27 13:19:06 -0500

    comment palindrome

--- src/problem5/mod.rs
+++ src/problem5/mod.rs
@@ -1,22 +1,35 @@
 use std::cmp::min;
 
 use crate::Solution;
+
 struct Longest {
-    left: usize,
-    right: usize,
-    size: usize,
+    left: usize, // left index (inclusive)
+    right: usize, // right index (inclusive)
+    size: usize, // total size of string
 }
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
+
+        // if the string is 1 length or shorter it is automatically a palindrome.
+        // this is also important because a lot of the code assumes the string is at least length 1
         if s.len() <= 1 {
             return s;
         }
+
+        // so we can grab a char by its index
         let chars: Vec<_> = s.chars().collect();
-        let len = s.len();
+
+        // tracks the longest string. Initially 0-sized. Since we know the string has more than one character,
+        // it must get a valid value
         let mut longest: Longest = Longest { left: 0, right: 0, size: 0 };
+
+        // the max index of the string
         let max_i = s.len() - 1;
-        for i in 0..len {
+
+        for i in 0..s.len() {
+
+            // odd-length palindromes (has one character as a center)
             let pivot = i;
             let max_one_side = min(max_i - i, i);
             let mut one_side_len = 0;
@@ -37,8 +50,13 @@ impl Solution {
                 }
             }
 
+            // even-length palindromes. This is i32 instead of usize as there are some cases where max_i - i - 1 will
+            // be less than 0 and this is the easiest way to handle that
             let max_one_side: i32 = min((max_i - i) as i32 - 1, i as i32);
 
+            // in the odd-length counting one-sided length did not count center, but this one does
+            // for example in odd-length a one_side_len of 0 would be one character, where this would
+            // be ""
             let mut one_side_len = 0;
             for j in 0..=max_one_side {
                 let j = j as usize;
@@ -52,7 +70,7 @@ impl Solution {
             let size = one_side_len * 2;
             if size > longest.size {
                 longest = Longest {
-                    left: pivot - one_side_len + 1,
+                    left: pivot - one_side_len + 1, // +1 does not make sense for OSL = 0, but consider all other OSLs
                     right: pivot + one_side_len,
                     size,
                 }
@@ -70,6 +88,6 @@ mod tests {
     #[test]
     fn it_works() {
         let palind = Solution::longest_palindrome("abccbracecar".to_string());
-        println!("palind {}", palind)
+        assert_eq!(palind, "racecar".to_string());
     }
 }
