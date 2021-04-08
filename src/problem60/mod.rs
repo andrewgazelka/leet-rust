9e43d577b6d9d2eaef34bd9679873e2abf0131a2
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-04-07 20:35:25 -0500

    use compile-time generated factorials

--- src/problem60/mod.rs
+++ src/problem60/mod.rs
@@ -1,26 +1,32 @@
 use crate::Solution;
 
+const MAX_N: usize = 9;
+
+const fn factorial_generator() -> [usize; MAX_N + 1] {
+    let mut arr = [0_usize; MAX_N + 1];
+    arr[0] = 1;
+    let mut i = 1;
+    while i <= MAX_N {
+        arr[i] = i * arr[i - 1];
+        i += 1;
+    }
+    arr
+}
+
+const FACTORIALS: [usize; MAX_N + 1] = factorial_generator();
+
 impl Solution {
     pub fn get_permutation(n: i32, k: i32) -> String {
         let n = n as usize;
 
         let mut str = String::with_capacity(n);
 
-        let factorials = {
-            let mut temp = Vec::with_capacity(n);
-            temp.push(1);
-            for i in 1..=n {
-                temp.push((i) * temp[i - 1]);
-            }
-            temp
-        };
-
         let mut k_left = k as usize - 1; // 0-indexed
 
         let mut available_numbers: Vec<_> = (1..=n as u8).collect();
 
         for factorial_idx in (0..n).rev() {
-            let factorial = factorials[factorial_idx];
+            let factorial = FACTORIALS[factorial_idx];
 
             let num_to_push_idx = k_left / factorial;
             let num_to_push = available_numbers.remove(num_to_push_idx);
