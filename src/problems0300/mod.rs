a3766e0e7594232a4beda94b47f22ef3e0f21713
Andrew Gazelka <andrew.gazelka@gmail.com>
2021-05-23 14:43:50 -0500

    add 384

--- src/problems0300/mod.rs
+++ src/problems0300/mod.rs
@@ -1,4 +1,5 @@
 mod p29;
 mod p38;
+mod p84;
 mod p87;
 mod p95;
---
+++ src/problems0300/p84.rs
@@ -0,0 +1,38 @@
+use rand::seq::SliceRandom;
+
+struct Solution {
+    original: Vec<i32>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl Solution {
+
+    fn new(original: Vec<i32>) -> Self {
+        Solution {
+           original
+        }
+    }
+
+    /** Resets the array to its original configuration and return it. */
+    fn reset(&self) -> Vec<i32> {
+        self.original.clone()
+    }
+
+    /** Returns a random shuffling of the array. */
+    fn shuffle(&self) -> Vec<i32> {
+        let mut rng = rand::thread_rng();
+        let mut array = self.original.clone();
+        array.shuffle(&mut rng);
+        array
+    }
+}
+
+/*
+ * Your Solution object will be instantiated and called as such:
+ * let obj = Solution::new(nums);
+ * let ret_1: Vec<i32> = obj.reset();
+ * let ret_2: Vec<i32> = obj.shuffle();
+ */
